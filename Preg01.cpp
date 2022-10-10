1d7a6e1f1e4609e3c71ef8fcd0e7c417d429992f
DiegoCasas12 <20a3010980@untels.edu.pe>
2022-10-10 09:56:45 -0500

    Pregunta01 -Listo por Casas

---
+++ Preg01.cpp
@@ -0,0 +1,29 @@
+Código Preg01
+
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+
+using namespace std;
+int main()
+{
+    int aux, a, b;
+    
+    cout <<"Ingrese el valor de a: ";cin>>a;
+    cout <<"Ingrese el valor de b: ";cin>>b;
+
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+
+    cout << "Intercambiando los valores" << endl;
+
+    aux = a;
+    a = b;
+    b = aux;
+
+    cout <<   "Ahora, a es: " << a << endl;
+    cout <<   "Ahora, b es: " << b << endl;
+
+    return 0;
+}
+
