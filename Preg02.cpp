3a985ce391855e518cb6836403d89f635c011cb7
DiegoCasas12 <20a3010980@untels.edu.pe>
2022-10-10 10:15:17 -0500

    Listo por Casas

---
+++ Preg02.cpp
@@ -0,0 +1,15 @@
+Código Preg02
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	int i,n, suma=0;
+	cout <<"Ingrese el valor de n: ";cin>>n;
+	for(i=1; i<n;i++){
+		suma=suma+i;
+	}
+	cout <<"El valor de la suma es: "<<suma;
+	return 0;
+}
+
